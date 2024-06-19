8855deaf5d310182d5fd1561d9ed2afdbda57f08
antonis08 <150551998+antonis08@users.noreply.github.com>
2024-06-19 23:00:01 +0300

    Delete Linos directory

--- Linos/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.dropdown').forEach(item => {
-    item.addEventListener('mouseover', () => {
-        item.querySelector('.dropdown-menu').classList.add('show');
-    });
-    item.addEventListener('mouseout', () => {
-        item.querySelector('.dropdown-menu').classList.remove('show');
-    });
-});
